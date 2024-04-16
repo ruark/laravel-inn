0f7cdd32a004dda7484c167ba9171d0fdd66720d
Yurii Ruark <y.lysianskii@uplati.ru>
2024-04-16 11:59:40 +0800

    Initial Commit

---
+++ src/InnValidatorServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ruark\LaravelInn;
+
+use Illuminate\Support\ServiceProvider;
+
+class InnValidatorServiceProvider extends ServiceProvider
+{
+    public function boot():void
+    {
+
+    }
+}
